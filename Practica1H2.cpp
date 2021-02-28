a0deec79e1a3cc68730451ba596f22668ac71fa8
CraniumAdamantium <64380067+CraniumAdamantium@users.noreply.github.com>
2021-02-27 20:45:56 -0400

    ArrayEstatico

--- Practica1H2.cpp
+++ Practica1H2.cpp
@@ -4,299 +4,250 @@
 #include <time.h>
 #include <algorithm>
 #include <conio.h>
-//hacer menú para generar los vectores y no repetir, hacerlo en una clase
+#define tam 100
+int v1[tam], v2[tam], v3[tam], cantidad, como;
 void limpiar()
 {
-    
+
     int n;
     for (n = 0; n < 10; n++)
-        printf( "\n\n\n\n\n\n\n\n\n\n" );
+        printf("\n\n\n\n\n\n\n\n\n\n");
 }
+
 void menu()
 {
-    
     limpiar();
+    std::cout << "\t\t\tIngrese una opción" << std::endl
+              << "0)\tSalir." << std::endl
+              << "1)\tSumar elementos de 2 vectores" << std::endl
+              << "2)\tElevar al cuadrado elementos de un vector" << std::endl
+              << "3)\tInvertir un vector y almacenarlo en otro vector" << std::endl
+              << "4)\tVer si el vector está ordenado de manera ascendente" << std::endl
+              << "5)\tMultiplicar el primer elemento por el último sucesivamente y almacenar en otro vector" << std::endl
+              << "6)\tConcatenar 2 vectores y ordenarlos ascendentemente" << std::endl;
+}
+//Para preguntar si es manual o aleatorio
+
+void listar(int array[], int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        std::cout << array[i] << "\t";
+    }
+    std::cout << std::endl;
+}
+void comoLlenar()
+{
     std::cout << "Ingrese una opción" << std::endl
-              << "0) salir." << std::endl
-              << "1) suma de 2 vectores aleatorios en un tercer vector" << std::endl
-              << "2)Llenar un vector de N elementos, imprimir la posición y el valor del elemento mayor almacenado en el vector. Suponga que todos los elementos del vector son diferentes." << std::endl
-              << "3)Almacenar N números en un vector, elevar al cuadrado cada valor almacenado en el vector, almacenar el resultado en otro vector. Imprimir el vector original y el vector resultante." << std::endl
-              << "4)Almacenar N números en un vector, imprimir cuantos son ceros, cuantos son negativos, cuantos positivos. Imprimir además la suma de los negativos y la suma de los positivos." << std::endl
-              << "5)Almacenar N números en un vector, almacenarlos en otro vector en orden inverso al vector original e imprimir el vector resultante." << std::endl
-              << "6)Se tienen almacenados en la memoria dos vectores A y B de N elementos cada uno. Hacer un algoritmo que escriba la palabra “Iguales” si ambos vectores son iguales y “Diferentes” si no lo son. Serán iguales cuando en la misma posición de ambos vectores se tenga el mismo valor para todos los elementos." << std::endl
-              << "7)Se tiene el vector A con N elementos almacenados. Diseñe un algoritmo que escriba “SI” si el vector está ordenado ascendentemente o “NO” si el vector no está ordenado." << std::endl
-              << "8)Diseñe un algoritmo que lea un número cualquiera y lo busque en el vector V, el cual tiene almacenados N elementos. Escribir la posición donde se encuentra almacenado el número en el vector o el mensaje “NO” si no lo encuentra. Búsqueda secuencial." << std::endl
-              << "9)Diseñe un algoritmo que lea dos vectores A y B de N elementos cada uno y multiplique el primer elemento de A con el último elemento de B y luego el segundo elemento de A por el N-1 elemento de B y así sucesivamente hasta llegar al N elemento de A por el primer elemento de B. El resultado de la multiplicación almacenarlo en un vector C." << std::endl
-              << "10)Dado un vector A de N elementos, hallar:" << std::endl
-              << "a.  La suma de los números pares" << std::endl
-              << "b.	La suma de los números impares" << std::endl
-              << "c.	La suma de los números primos" << std::endl
-              << "d.	La cantidad de números pares, impares y primos." << std::endl;
+              << "1)Manual" << std::endl
+              << "2)Aleatorio" << std::endl;
+}
+//Función para llenar el array
+void llenarArray(int *array, int cantidad, int como)
+{
+    if (cantidad > 100)
+    {
+        std::cout << "Valor demasiado grande" << std::endl;
+        return;
+    }
+    else
+    {
+        int valor;
+        if (como == 1)
+        {
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                std::cout << "Ingrese valor para la posición " << i << " : ";
+                std::cin >> valor;
+                std::cout << std::endl;
+                array[i] = valor;
+            }
+        }
+        if (como == 2)
+        {
+            int valor, rini, rend;
+            std::cout << "Ingrese el rango aleatorio inicial" << std::endl;
+            std::cin >> rini;
+            std::cout << "Ingrese el rango aleatorio final" << std::endl;
+            std::cin >> rend;
+            rend++;
+            srand(time(0));
+            for (int i = 0; i < cantidad; i++)
+            {
+                array[i] = rand() % rend + rini;
+            }
+        }
+    }
 }
 
 int main()
 {
-    int a, b, c, ex = 0, i;
+    int a, b, c, ex = 0, i, como;
     do
     {
         menu();
         std::cin >> ex;
         switch (ex)
         {
-
         case 1:
         {
-            std::cout << "Ingrese el tamaño de los vectores A y B: ";
-            std::cin >> a;
-            int *v1 = new int[a];
-            int *v2 = new int[a];
-            int *v3 = new int[a];
-            for (i = 0; i < a; i++)
+            std::cout << "Ingrese el tamaño de los vectores" << std::endl;
+            std::cin >> cantidad;
+            comoLlenar();
+            std::cin >> como;
+            std::cout << "Para vector A" << std::endl;
+            llenarArray(v1, cantidad, como);
+            std::cout << "Para vector B" << std::endl;
+            llenarArray(v2, cantidad, como);
+            for (i = 0; i < cantidad; i++)
             {
-                v1[i] = rand() % 99 + 1;
-                std::cout << "V1 = " << v1[i];
-                v2[i] = rand() % 30 + 10;
-                std::cout << ", V2 = " << v2[i];
                 v3[i] = v1[i] + v2[i];
-                std::cout << " : V3 = " << v3[i] << std::endl;
             }
-            getch();
-        }
-        break;
-
-        case 2:
-        {
-            std::cout << "Ingrese el tamaño del vector : ";
-            std::cin >> a;
-            int *v1 = new int[a];
-            for (i = 0; i < a; i++)
-            {
-                v1[i] = rand() % 200 + 10;
-                std::cout << i << " : " << v1[i] << std::endl;
-            }
-            b = 0;
-            int contador = 0;
-            for (i = 0; i < a; i++)
-            {
-                c = v1[i];
-                if (c > b)
-                {
-                    b = c;
-                    contador = i;
-                }
-            }
-            std::cout << std::endl;
-            std::cout << "número más alto en la posición : " << contador << " con valor de : " << b;
+            std::cout << "El vector resultante es:" << std::endl;
+            listar(v3, cantidad);
             getch();
             break;
         }
-
+        case 2:
+        {
+            std::cout << "Ingrese el tamaño del vector";
+            std::cin >> cantidad;
+            comoLlenar();
+            std::cin >> como;
+            llenarArray(v1, cantidad, como);
+            std::cout << "Original:" << std::endl;
+            listar(v1, cantidad);
+            for (int i = 0; i < cantidad; i++)
+            {
+                v2[i] = v1[i] * v1[i];
+            }
+            std::cout << std::endl;
+            std::cout << "Al cuadrado: " << std::endl;
+            listar(v2, cantidad);
+            getch();
+            break;
+        }
         case 3:
         {
-            std::cout << "Ingrese el tamaño del vector : ";
-            std::cin >> a;
-            double *v1 = new double[a];
-            double *v2 = new double[a];
-            std::cout << "Num : Sqrt" << std::endl;
-            for (i = 0; i < a; i++)
+            std::cout << "Ingrese el tamaño del vector" << std::endl;
+            std::cin >> cantidad;
+            comoLlenar();
+            std::cin >> como;
+            llenarArray(v1, cantidad, como);
+            std::cout << "Original" << std::endl;
+            listar(v1, cantidad);
+            for (int i = 0; i < cantidad; i++)
             {
-                v1[i] = rand() % 100 + 10;
-                std::cout << v1[i];
-                v2[i] = sqrt(v1[i]);
-                std::cout << " : " << v2[i] << std::endl;
+                v2[i] = v1[cantidad - i - 1];
             }
+            std::cout << std::endl;
+            std::cout << "Inverso" << std::endl;
+            listar(v2, cantidad);
             getch();
             break;
         }
         case 4:
         {
-            srand(time(0));
-            std::cout << "Ingrese el tamaño del vector : ";
-            std::cin >> a;
-            int *v1 = new int[a];
-            int nega = 0, posi = 0, cero = 0;
-            int sumanega = 0, sumaposi = 0;
-
-            for (i = 0; i < a; i++)
+            std::cout << "Ingrese el tamaño del vector" << std::endl;
+            std::cin >> cantidad;
+            comoLlenar();
+            std::cin >> como;
+            llenarArray(v1, cantidad, como);
+            std::cout << "Original" << std::endl;
+            listar(v1, cantidad);
+            int f;
+            for (int i = 0; i < cantidad; i++)
             {
-                v1[i] = (rand() % +201) - 100;
-                std::cout << v1[i] << " : ";
-                if (v1[i] == 0)
-                    cero++;
-                if (v1[i] > 0)
+                if (v1[0] > v1[i])
                 {
-                    posi++;
-                    sumaposi = sumaposi + v1[i];
+                    f = 1;
+                    break;
                 }
-                if (v1[i] < 0)
+                else
                 {
-                    nega++;
-                    sumanega = sumanega + v1[i];
+                    f = 0;
                 }
             }
-            std::cout << std::endl
-                      << "Hay " << cero << " cero(s), " << posi << " positivos y " << nega << " negativos" << std::endl;
-            std::cout << "La suma de positivos es: " << sumaposi << " y la suma de negativos es : " << sumanega << std::endl;
+            if (f == 1)
+            {
+                std::cout << "El vector NO está ordenado de manera ascendente" << std::endl;
+            }
+            else
+            {
+                std::cout << "El vector está ordenado de manera ascendente" << std::endl;
+            }
             getch();
             break;
         }
         case 5:
         {
-            std::cout << "Ingrese el tamaño del vector" << std::endl;
-            std::cin >> a;
-            int *v1 = new int[a];
-            int *v2 = new int[a];
-            srand(time(0));
-            std::cout << "Este es el vector aleatorio de " << a << " números" << std::endl;
-            for (i = 0; i < a; i++)
+            std::cout << "Ingrese el tamaño de los vectores" << std::endl;
+            std::cin >> cantidad;
+            comoLlenar();
+            std::cin >> como;
+            std::cout << "Para vector A" << std::endl;
+            llenarArray(v1, cantidad, como);
+            std::cout << "Para vector B" << std::endl;
+            llenarArray(v2, cantidad, como);
+            for (int i = 0; i < cantidad; i++)
             {
-                v1[i] = rand() % 100 + 1;
-                std::cout << v1[i] << " ";
+                v3[i] = v1[i] * v2[cantidad - i - 1];
             }
-            std::cout << std::endl
-                      << "Este es el vector inverso " << std::endl;
-            int cont = 0;
-            for (i = a - 1; i > -1; i--)
-            {
-                v2[cont] = v1[i];
-                cont++;
-            }
-            for (i = 0; i < a; i++)
-            {
-                std::cout << v2[i] << " ";
-            }
-            std::cout << std::endl;
+            std::cout << "Vectora A" << std::endl;
+            listar(v1, cantidad);
+            std::cout << "Vector B" << std::endl;
+            listar(v2, cantidad);
+            std::cout << "Vector C" << std::endl;
+            listar(v3, cantidad);
             getch();
             break;
         }
         case 6:
         {
-            std::cout << "Ingrese el tamaño del vector " << std::endl;
-            std::cin >> a;
-            int *v1 = new int[a];
-            int *v2 = new int[a];
-            srand(time(0));
-            for (i = 0; i < a; i++)
+            int cantidad2;
+            std::cout << "Ingrese el tamaño del vector A :";
+            std::cin >> cantidad;
+            std::cout << "Ingrese el tamaño del vector B :";
+            std::cin >> cantidad2;
+            if (cantidad + cantidad2 >= 100)
             {
-                v1[i] = rand() % 10 + 1;
-                v2[i] = rand() % 10 + 1;
-                if (v1[i] == v2[i])
-                {
-                    std::cout << "Posición Nº : " << i << " : " << v1[i] << "-" << v2[i] << " : SON IGUALES" << std::endl;
-                }
-                else
-                {
-                    std::cout << "Posición Nº : " << i << " : " << v1[i] << "-" << v2[i] << " : SON DIFERENTES" << std::endl;
-                }
+                std::cout << "Demasiados valores" << std::endl;
+                break;
             }
+            comoLlenar();
+            std::cin >> como;
+            std::cout << "Para vector A" << std::endl;
+            llenarArray(v1, cantidad, como);
+            std::cout << "Para vector B" << std::endl;
+            llenarArray(v2, cantidad2, como);
+            std::cout << "Vector A SIN ORDENAR : " << std::endl;
+            listar(v1, cantidad);
+            std::cout << "Vector B SIN ORDENAR : " << std::endl;
+            listar(v2, cantidad2);
+            std::sort(std::begin(v1), std::begin(v1) + cantidad);
+            std::cout << "Vector A ordenado ascendentemente : " << std::endl;
+            listar(v1, cantidad);
+            std::cout << "Vector B ordenado ascendentemente : " << std::endl;
+            std::sort(std::begin(v2), std::begin(v2) + cantidad2);
+            listar(v2, cantidad2);
+            for (int i = 0; i < cantidad; i++)
+            {
+                v3[i] = v1[i];
+            }
+            for (int i = cantidad; i < cantidad + cantidad2; i++)
+            {
+                v3[i] = v2[i - cantidad];
+            }
+            std::sort(std::begin(v3), std::begin(v3) + (cantidad + cantidad2));
+            std::cout << "Vector C ordenado ascendentemente : " << std::endl;
+            listar(v3, cantidad + cantidad2);
             getch();
             break;
         }
         case 7:
         {
-            std::cout << "Ingrese el tamaño del vector :";
-            std::cin >> a;
-            std::vector<int> v1;
-            srand(time(0));
-            std::cout << "Generando vector aleatorio con valores: 1-100" << std::endl;
-            for (i = 0; i < a; i++)
-            {
-                v1.push_back(rand() % 100 + 1);
-                std::cout << v1[i] << " ";
-            }
-            int cheq = rand() % 2 + 1;
-            if (cheq == 1)
-            {
-                std::sort(std::begin(v1), std::end(v1));
-            }
-            std::cout << "Randomizando si ordenar o no..." << std::endl; //
-            //aquí comienza el algoritmo para chequear si está ordenado de forma ascendente
-            c = v1[0];
-            int z = 0;
-            for (i = 0; i < a; i++)
-            {
-                std::cout << v1[i] << " ";
-                b = v1[i];
-                if (b >= c)
-                {
-                    z = 1;
-                }
-                else
-                {
-                    z = 0;
-                    break;
-                }
-            }
-            if (z == 1)
-            {
-                std::cout << "Está ordenado de manera ascendente." << std::endl;
-            }
-            else
-            {
-                std::cout << "NO está ordenado de manera ascendente." << std::endl;
-            }
-            getch();
-            break;
-        }
-        case 8:
-        {
-            std::cout << "Ingrese el tamaño del vector" << std::endl;
-            std::cin >> a;
-            std::vector<int> v1;
-            for (i = 0; i < a; i++)
-            {
-                v1.push_back(rand() % (100+1) - 1);
-            }
-            std::cout << "Generando números aleatorios de 1 al 100..." << std::endl
-                      << "Ingrese un número del 1 al 100 para ver si se genero de manera aleatoria en el vector." << std::endl;
-            int val;
-            std::cin >> val;
-            std::cout << "Mostrando los números generados aleatoriamente." << std::endl;
-            for (auto l : v1)
-            {
-                std::cout << l << " ";
-            }
-            std::cout << std::endl;
-            for (i = 0; i < v1.size(); i++)
-            {
-                if (v1[i] == val)
-                {
-                    std::cout << "Se encontró el número en la posición " << i << std::endl;
-                    break;
-                }
-                else
-                {
-                    std::cout << "No se encontró el número en la posición :" << i << std::endl;
-                }
-            }
-            getch();
-            break;
-        }
-        case 9:
-        {
-            std::cout << "Ingrese el tamaño de los vectores" << std::endl;
-            std::cin >> a;
-            std::vector<int> v1, v2, v3;
-            srand(time(0));
-            std::cout << "Generando dos vectores aleatorios..." << std::endl;
-            for (i = 0; i < a; i++)
-            {
-                v1.push_back(rand() % (100+1) - 1);
-                std::cout << "v1 posición :" << i << " valor :" << v1[i] << "    ";
-                v2.push_back(rand() % 100 - 1);
-                std::cout << "v2 :" << i << " valor :" << v2[i] << std::endl;
-            }
-            std::cout << "Multiplicando los vectores..." << std::endl;
-            for (i = 0; i < a; i++)
-            {
-                v3.push_back(v1[i] * v2[a - i - 1]);
-                std::cout << "v3 posición :" << i << " valor :" << v3[i] << std::endl;
-            }
-            getch();
-            break;
-        }
-        case 10:
-        {
+            
         }
         default:
             std::cout << "Ingrese una opción correcta";
