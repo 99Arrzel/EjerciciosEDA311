bff6d2ab180143e64dfee384f915090f9f5e8979
CraniumAdamantium <64380067+CraniumAdamantium@users.noreply.github.com>
2021-04-01 03:40:13 -0400

    teletype

---
+++ encriptrucho.cpp
@@ -0,0 +1,252 @@
+//facilito 
+#include <iostream> //cout
+#include <conio.h> //getch
+//#include <vector> // casi lo uso
+#include <string> //getline
+#include <math.h> //abs
+#include <cstring> //memset
+#include <time.h>  //time
+using std::cout; //Using std::cout para evitar escribir tanto std...
+
+//Variables Globales, hay algunas más que deberían serlo...
+std::string m1, password, passwordIngresada;
+int v[100][100], passwordInt, passwordIngresadaInt, v2[100][100];
+int icounter = 0, jcounter = 0, mezclar = 0, contadorLetras = 0, endvalue = 0;
+///////////////////////
+/*---HACKEO---, la función llena de valores aleatorios los arrays bidimensionales*/
+void hackeo()
+{
+   srand(time(NULL));
+   for (int i = 0; i < 100; i++)
+   {
+      for (int j = 0; j < 100; j++)
+      {
+         v2[i][j] = rand() % 99999 + 10000;
+         v[i][j] = rand() % 99999 + 10000;
+      }
+   }
+}
+/*----MENSAJE----, la función llama a getline para obtener el valor del mensaje*/
+void mensaje()
+{
+   //
+   cout << "Ingrese el mensaje" << std::endl;
+   std::cin.ignore();
+   std::getline(std::cin, m1);
+   //
+}
+/*----OBTENERPASSWORD----, la función llama a getline para obtener el valor del PASSWORD, además, obtiene su valor int de la suma de sus valores ASCII y se multiplica por si mismo*/
+void obtenerPassword()
+{
+   passwordInt = 0;
+   //
+   cout << "Ingrese la contraseña" << std::endl;
+   std::getline(std::cin, password);
+   for (int i = 0; i < password.size(); i++)
+   {
+      passwordInt += password[i]; //la suma de los valores ascii de la pass
+   }
+   passwordInt *= passwordInt; //multiplicado por si mismo
+   //
+}
+/*---ENCRIPTADO----, la función establece el memset del array v a 0, determina el tamaño del array con la raiz cuadrada del size de la cadena
+además, convierte dichos valores de nuestro string en valores ASCII*/
+void encriptado()
+{
+   memset(v, 0, sizeof(v));
+   char m = '0';
+   int oracionSize = m1.size();
+   int contador = 0;
+   for (int i = 0; i < sqrt(m1.size()); i++) //Transcribimos los valores ascii en la matriz
+   {
+
+      for (int j = 0; j < sqrt(m1.size()); j++)
+      {
+         if (contador <= oracionSize)
+         {
+            v[i][j] = ((int(m1[contador])));
+            contador++;
+         }
+         cout << v[i][j] << "\t\t";
+      }
+      cout << std::endl;
+      cout << std::endl;
+   }
+   cout << "------------------1-------------------" << std::endl;
+   contador = 0;                             // resetear contador para futuros mensajes
+    /*"Encriptamos" sumando la contraseña para cada valor y lo mostramos, es el PRIMER PASO*/
+   for (int i = 0; i < sqrt(m1.size()); i++)
+   {
+      for (int j = 0; j < sqrt(m1.size()); j++)
+      {
+         v[i][j] += passwordInt;
+         cout << v[i][j] << "\t\t";
+      }
+      cout << std::endl;
+      cout << std::endl;
+   }
+   cout << "-----------------2----------------------" << std::endl;
+   int mezclar = passwordInt;
+   int limite = (sqrt(m1.length())) + 1;
+   icounter = 0;
+   jcounter = 0;
+   srand(time(NULL));
+   contadorLetras = m1.length();
+   /*"Encriptamos" mezclamos los valores dependiendo de lacontraseña y creamos BAIT con valores aleatorios para despistar*/
+   for (int i = 0; i < (sqrt(m1.size())) * 2; i++) 
+   {
+
+      for (int j = 0; j < (sqrt(m1.size())) * 2; j++)
+      {
+         v2[i][j] = rand() % 99999 + 10000;
+         if (mezclar % 2 != 0)
+         {
+            if (jcounter < limite && v[icounter][jcounter] != 0)
+            {
+               v2[i][j] = v[icounter][jcounter];
+               jcounter++;
+
+               if (jcounter == limite)
+               {
+                  jcounter = 0;
+                  icounter++;
+               }
+            }
+         }
+         mezclar--;
+         cout << v2[i][j] << "\t\t";
+      }
+      cout << std::endl;
+      cout << std::endl;
+   }
+   double endvalue2;
+   endvalue2 = (sqrt(m1.size()) * 2);
+   /*Establecemos si nuestra raiz fue irracional o no, con su valor absoluto, para posicionarlo en última o penultima posición, así sabremos el LENGTH de nuestro array que estará en el propio código*/
+   if (endvalue2 == abs(endvalue2) && endvalue2 > 5)
+   {
+      cout << "Absoluto no es endvalue\n";
+      endvalue = (sqrt(m1.size()) * 2) - 1; 
+   }
+   else
+   {
+      endvalue = (sqrt(m1.size()) * 2); 
+   }
+
+   v2[endvalue][endvalue] = contadorLetras; 
+   cout << "\nEl mensaje es :\n";
+   /*Hacemos un OUTPUT de los valores de nuestro ARRAY BIDIMENSIONAL, este ya es el mensaje encriptado*/
+   for (int i = 0; i < (sqrt(m1.size())) * 2; i++) //cout valores
+   {
+      for (int j = 0; j < (sqrt(m1.size())) * 2; j++)
+      {
+         cout << v2[i][j];
+      }
+   }
+   endvalue = 0;
+}
+/*------------DESENCRIPTADO---------Esta función sirve para desencriptar nuestro array bidimensional*/
+void desencriptado()
+{
+   if (passwordInt == 0) //comprobación de password, si no la hay no deja continuar
+   {
+      cout << "Debes encriptar primero" << std::endl;
+      return;
+   }
+   srand(time(NULL));
+   int hackeadoPapa = rand() % 5 + 1;
+   if (hackeadoPapa == 1) //Comprobación de hackeo, es al azar con una chance de 1/5(20%) de ser hackeado
+   {
+      hackeo();
+      cout << "\nEl mensaje fue hackeado y por lo tanto es ilegible.\n";
+      passwordInt = 0;
+      return;
+   }
+   cout << "\nIngresa la contraseña" << std::endl; //Pedimos la contraseña para iniciar el proceso de desencriptado
+   std::cin >> passwordIngresada;
+   passwordIngresadaInt = 0;
+   //Contraseña
+   for (int i = 0; i < passwordIngresada.length(); i++)
+   {
+      passwordIngresadaInt += int(passwordIngresada[i]);
+   }
+   passwordIngresadaInt *= passwordIngresadaInt;
+   //
+   mezclar = passwordIngresadaInt;
+   double endvalue2 = (sqrt(m1.size()) * 2);
+   if (endvalue2 == abs(endvalue2) && endvalue2 > 5) // Comprobamos igual que en el encriptado, si nuestro cuadrado es irracional o no
+   {
+      endvalue = (sqrt(m1.size()) * 2) - 1; 
+   }
+   else
+   {
+      endvalue = (sqrt(m1.size()) * 2); 
+   }
+   contadorLetras = v2[endvalue][endvalue];
+   //Establecemos un valor char inicializado para desencriptar valores ASCII
+   char ch = '0';
+   //
+   for (int i = 0; i < (sqrt(m1.size())) * 2; i++) //Desencriptamos
+   {
+      for (int j = 0; j < (sqrt(m1.size())) * 2; j++)
+      {
+         if (mezclar % 2 != 0 && contadorLetras > 0) //Mezclar es el valor de nuestra contraseña en int, si es un número par pasa algo, si es un impar pasa otra cosa
+         {
+            if (v2[i][j] - passwordIngresadaInt != 0)
+            {
+               ch = char(v2[i][j] - passwordIngresadaInt); //Output de nuestro valor desencriptado
+               std::cout << ch;
+            }
+
+            contadorLetras--;
+         }
+         mezclar--;
+      }
+   }
+}
+void limpiar() //Función para limpiar y mostrar mensaje
+{
+   cout << "\nPresione una tecla para continuar" << std::endl;
+   getch();
+   system("cls");
+}
+
+void menu()//Función del menú
+{
+   cout << "1)Envio de Mensajería\n";
+   cout << "2)Recepción de Mensajería\n";
+   cout << "3)Salir\n";
+}
+int opt = 0;
+/*Main en el cual daremos lugar a todos nuestros procesos*/
+int main()
+{
+   do
+   {
+      menu();
+      std::cin >> opt;
+      switch (opt)
+      {
+      case 1:
+      {
+         mensaje();
+         obtenerPassword();
+         encriptado();
+         limpiar();
+         break;
+      }
+      case 2:
+      {
+         desencriptado();
+         limpiar();
+         break;
+      }
+      case 3:
+      {
+         opt = 0;
+      }
+      default:
+         break;
+      }
+   } while (opt != 0);
+   return 0;
+}
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,108 @@
+
 print("Hello World")
+if 5 > 2:
+    print("5 es mayor que 2")
+if 5 > 2:
+    print("con espacios")
+x = 5
+y = "hello"
+print(y)
+# comentar?¿
+"""
+comentar
+"""
+x = int(3)
+print (type(x))
+x,y,z = "Naranja", "Banana", "Cherry"
+print (x,y,z)
+x = z = y = "Naranja"
+print (x,y,z)
+frutas = ["Manzana", "Banana", "Cherry"]
+x,y,z = frutas #unpacking
+print (x,y,z)
+x = "XD"
+print ("Disney" + x) #imprimir
+x = "Python es facilito "
+y = "EZ"
+z = x + y #concatenar
+print (z)
+x = 5
+y = 10
+print (x+y) #suma en print
+x = "10"
+def myFunc(): #define
+    print("Python vale " + x)
+
+myFunc() #ejecuta
+def myFunc2():
+    global x #declarado global
+    x = "Facilitooo" #debe asignarse después el valor
+    print ("Python es " + x)
+myFunc2()
+print("Python es que??? " + x)
+z = complex(2, -3)
+print(z)
+x = 35e3
+y = 12E4
+z = -87.7e100
+x = int(x)
+print(type(x))
+print(y)
+print(z)
+import random
+print (random.randrange(1,10))
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+a = "Hello, World!"
+print(a[1])
+for x in "banana":
+  print(x)
+a = "Hello, World!"
+print(len(a))
+txt = "The best things in life are free!"
+print("free" not in txt)
+a = "Hello, World!"
+print(a.upper())
+a = " Hello, World! "
+print(a.strip())#de principio o final
+a = "Hello, World!"
+print(a.replace("H", "J"))
+a = "Hello, World!"
+print(a.split(",")) #, is target
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon") #desplaza al 2
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)#concatena listas 
+print(thislist)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]#newlist = [expression for item in iterable if condition == True]
+
+print(newlist)
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()#limpiar lista
+print(thislist)
+newlist = [x.upper() for x in newlist]#A mayuscula
+print(newlist)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+thislist.reverse()
+print(thislist)
